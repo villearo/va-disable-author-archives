d3af8086ea94437547ebefeeb55cc878a58f7187
Ville Aro <ville_aro@icloud.com>
2018-03-17 16:00:28 +0100

    test

--- va-disable-author-archives.php
+++ va-disable-author-archives.php
@@ -21,7 +21,6 @@ function va_disable_author_archives() {
         status_header(404);
         nocache_headers();
 		get_template_part( 404 );
-		exit();
     } else {
         redirect_canonical();   
     }
